0c6227801a74cd2ae3e0b2f96a116561db5e7618
Manish <manishrajak497229@gmail.com>
2025-06-29 22:54:26 +0530

    deployment link update

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL="http://localhost:5600/api/"
+const BASE_URL="https://file-trans.onrender.com/api/"
 const axiosInstance=axios.create()
 
 axiosInstance.defaults.baseURL=BASE_URL;
